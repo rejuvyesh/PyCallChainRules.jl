b34ff74e62d5967d4ce51dc26d2caa5f6b0dc845
rejuvyesh <mail@rejuvyesh.com>
2022-01-20 21:51:15 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/PyCallChainRules.jl
@@ -0,0 +1,5 @@
+module PyCallChainRules
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PyCallChainRules
+using Test
+
+@testset "PyCallChainRules.jl" begin
+    # Write your tests here.
+end
