2e9dbcbefc0f1f8fb7852ba85ebf5ce5d963d42e
rejuvyesh <mail@rejuvyesh.com>
2022-01-21 00:19:21 +0000

    zygote seems happy

--- test/test_pytorch.jl
+++ test/test_pytorch.jl
@@ -2,6 +2,7 @@ using PyCallChainRules.Torch: TorchModuleWrapper, torch, functorch
 
 using Test
 using ChainRulesTestUtils
+using Zygote
 
 batchsize = 1
 indim = 3
@@ -14,4 +15,10 @@ x = randn(Float32, indim, batchsize)
 y = linwrap(x)
 @test size(y) == (outdim, batchsize)
 
-test_rrule(linwrap, x)
+# Zygote check
+grad,  = Zygote.gradient(m->sum(m(x)), linwrap)
+@test length(grad.params) == 2
+@test grad.params[1] !== nothing
+@test grad.params[2] !== nothing
+
+#test_rrule(linwrap, x)
