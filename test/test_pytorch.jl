f12b1bac9008f78b600049cc1319d9c43dd256b5
rejuvyesh <mail@rejuvyesh.com>
2022-01-21 00:42:17 +0000

    more shape tests

--- test/test_pytorch.jl
+++ test/test_pytorch.jl
@@ -20,6 +20,8 @@ grad,  = Zygote.gradient(m->sum(m(x)), linwrap)
 @test length(grad.params) == 2
 @test grad.params[1] !== nothing
 @test grad.params[2] !== nothing
+@test size(grad.params[1]) == size(linwrap.params[1])
+@test size(grad.params[2]) == size(linwrap.params[2])
 
 grad, = Zygote.gradient(z->sum(linwrap(z)), x)
 @test size(grad) == size(x)
