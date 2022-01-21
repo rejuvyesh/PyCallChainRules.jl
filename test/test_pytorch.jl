f89963fe0b335cbbd9481898732048f198d297e2
rejuvyesh <mail@rejuvyesh.com>
2022-01-21 20:56:14 +0000

    bring back tests

--- test/test_pytorch.jl
+++ test/test_pytorch.jl
@@ -2,12 +2,12 @@ using PyCallChainRules.Torch: TorchModuleWrapper, torch, functorch
 
 using Test
 using ChainRulesTestUtils
-#using Zygote
-#using Flux
+using Zygote
+using Flux
 using ChainRulesCore: NoTangent, AbstractZero
 import Random
 using PyCall
-using Infiltrator
+
 ChainRulesTestUtils.rand_tangent(rng::Random.AbstractRNG, x::Ptr) = NoTangent()
 ChainRulesTestUtils.test_approx(::AbstractZero, x::PyObject, msg=""; kwargs...) = @test true
 
@@ -30,7 +30,7 @@ x = randn(Float32, indim, batchsize)
 y = linwrap(x)
 @test size(y) == (outdim, batchsize)
 
-# CRTU check TODO
+# CRTU check
 x = randn(Float32, indim, batchsize)
 test_rrule(linwrap, x; check_inferred=false, check_thunked_output_tangent=false, atol=1e-4, rtol=1e-4)
 # const CRTU = ChainRulesTestUtils
@@ -59,20 +59,18 @@ test_rrule(linwrap, x; check_inferred=false, check_thunked_output_tangent=false,
 
 
 
-# # Zygote check
-# grad,  = Zygote.gradient(m->sum(m(x)), linwrap)
-# @test length(grad.params) == 2
-# @test grad.params[1] !== nothing
-# @test grad.params[2] !== nothing
-# @test size(grad.params[1]) == size(linwrap.params[1])
-# @test size(grad.params[2]) == size(linwrap.params[2])
+# Zygote check
+grad,  = Zygote.gradient(m->sum(m(x)), linwrap)
+@test length(grad.params) == 2
+@test grad.params[1] !== nothing
+@test grad.params[2] !== nothing
+@test size(grad.params[1]) == size(linwrap.params[1])
+@test size(grad.params[2]) == size(linwrap.params[2])
 
-# grad, = Zygote.gradient(z->sum(linwrap(z)), x)
-# @test size(grad) == size(x)
+grad, = Zygote.gradient(z->sum(linwrap(z)), x)
+@test size(grad) == size(x)
 
-# # Flux check
-# nn = Chain(Dense(4, 3), linwrap)
-# x2 = randn(Float32, 4, batchsize)
-# grad,  = Zygote.gradient(m->sum(m(x2)), nn)
-
-
+# Flux check
+nn = Chain(Dense(4, 3), linwrap)
+x2 = randn(Float32, 4, batchsize)
+grad,  = Zygote.gradient(m->sum(m(x2)), nn)
