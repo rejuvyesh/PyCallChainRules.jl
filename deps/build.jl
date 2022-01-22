c47de570678baa060136bd185abb8ac87a765afc
rejuvyesh <mail@rejuvyesh.com>
2022-01-22 02:15:54 +0000

    try fix again

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,6 @@
 using PyCall
 
-pydeps = Dict(["torch" => "--pre torch -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html", "jax"=> "jax", "functorch"=>"git+https://github.com/pytorch/functorch.git"])
+pydeps = Dict(["torch" => ["--pre", "torch", "-f" ,"https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html"], "jax"=> "jax", "functorch"=>"git+https://github.com/pytorch/functorch.git"])
 for (dep, pip_suffix) in pydeps
     try
         pyimport(dep)
@@ -17,7 +17,7 @@ for (dep, pip_suffix) in pydeps
         - Rebuild `PyCall` to use Conda by running the following in Julia REPL
             - `ENV[PYTHON]=""; using Pkg; Pkg.build("PyCall"); Pkg.build("PyCallChainRules")
         - Or install the dependencies by running `pip`
-            - `pip install torch`
+            - `pip install $(pip_suffix)`
             """
                 )              
         end
