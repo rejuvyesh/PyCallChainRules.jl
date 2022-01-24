18c61198507819c665c13fb1651889b3b148c89d
rejuvyesh <mail@rejuvyesh.com>
2022-01-24 22:15:51 +0000

    test on pytorch hub

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,9 @@ using SafeTestsets
 @safetestset "PyCallChainRules.jl" begin
     @safetestset "torch" begin
         include("test_pytorch.jl")
+        @safetestset "hub" begin
+            include("test_pytorch_hub.jl")
+        end
     end
     @safetestset "jax" begin
         include("test_jax.jl")
