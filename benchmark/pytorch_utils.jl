e2ceace0e5115b54567e0e9ac134eb08c74fd58d
rejuvyesh <mail@rejuvyesh.com>
2022-01-25 22:28:37 +0000

    update benchmark suite

--- benchmark/pytorch_utils.jl
+++ benchmark/pytorch_utils.jl
@@ -1,5 +1,5 @@
 
-function forward_pass!(s, torchmodel, dummyinput, batchsize; samples=100)
+function forward_pass!(s, torchmodel, dummyinput, batchsize; samples=100, evals=10)
     ml = torchmodel
     mlfun, params, buffers = functorch.make_functional_with_buffers(deepcopy(torchmodel))
     mlfun2 = x->mlfun(params, buffers, x)
@@ -7,13 +7,13 @@ function forward_pass!(s, torchmodel, dummyinput, batchsize; samples=100)
 
     inshape = size(dummyinput)
     ## Torch
-    b_torch = @benchmarkable($ml(x), setup=(x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples)
+    b_torch = @benchmarkable($ml(x), setup=(x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples, evals=evals)
 
     ## Functorch
-    b_functorch = @benchmarkable($mlfun2(x), setup=( x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples)
+    b_functorch = @benchmarkable($mlfun2(x), setup=( x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples, evals=evals)
 
     ## TorchModuleWrapper
-    b_jl = @benchmarkable($mlwrap(x), setup=(x=randn(Float32, $(reverse(inshape))..., $batchsize)), samples=samples)
+    b_jl = @benchmarkable($mlwrap(x), setup=(x=randn(Float32, $(reverse(inshape))..., $batchsize)), samples=samples, evals=evals)
 
     # @info "Average slowdown vs torch" (mean(b_jl.times) / mean(b_torch.times))
     # @info "Average slowdown vs functorch" (mean(b_jl.times) / mean(b_functorch.times))
@@ -24,7 +24,7 @@ function forward_pass!(s, torchmodel, dummyinput, batchsize; samples=100)
     s["jl"] = b_jl
 end
 
-function backward_pass!(s, torchmodel, dummyinput, batchsize; samples=100)
+function backward_pass!(s, torchmodel, dummyinput, batchsize; samples=100, evals=10, seconds=20)
     ml = torchmodel
     mlfun, params, buffers = functorch.make_functional_with_buffers(deepcopy(torchmodel))
     mlfun2 = x->mlfun(params, buffers, x)
@@ -39,11 +39,11 @@ function backward_pass!(s, torchmodel, dummyinput, batchsize; samples=100)
     ## TorchModuleWrapper
     zygotegrad = (m, y) -> Zygote.gradient(m->sum(m(y)), m)
 
-    b_torch = @benchmarkable($torchgrad(m, x), setup=(m=$(torchmodel); x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples)
+    b_torch = @benchmarkable($torchgrad(m, x), setup=(m=$(torchmodel); x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples, evals=evals, seconds=seconds)
 
-    b_functorch = @benchmarkable($funcgrad(params, x), setup=(params=$params; x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples)
+    b_functorch = @benchmarkable($funcgrad(params, x), setup=(params=$params; x=torch.randn($batchsize, $(inshape)..., dtype=torch.float32)), samples=samples, evals=evals, seconds=seconds)
 
-    b_jl = @benchmarkable($zygotegrad(mlwrap, x), setup=(mlwrap=$mlwrap; x=randn(Float32, $(reverse(inshape))..., $batchsize)), samples=samples)
+    b_jl = @benchmarkable($zygotegrad(mlwrap, x), setup=(mlwrap=$mlwrap; x=randn(Float32, $(reverse(inshape))..., $batchsize)), samples=samples, evals=evals, seconds=seconds)
 
     # @info "Average slowdown vs torch" (mean(b_jl.times) / mean(b_torch.times))
     # @info "Average slowdown vs functorch" (mean(b_jl.times) / mean(b_functorch.times))
