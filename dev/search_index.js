f688c34b175d306d80c58db3ada1d038c0156786
Documenter.jl <documenter@juliadocs.github.io>
2022-01-25 06:01:40 +0000

    build based on 0e7b02e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PyCallChainRules","category":"page"},{"location":"#PyCallChainRules","page":"Home","title":"PyCallChainRules","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PyCallChainRules]","category":"page"}]
+}
