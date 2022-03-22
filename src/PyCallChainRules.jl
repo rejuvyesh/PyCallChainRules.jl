d39073d0667add14e0376730bdd09956d6e1506a
rejuvyesh <mail@rejuvyesh.com>
2022-03-22 02:15:16 +0000

    get DiffEqFlux example working

--- src/PyCallChainRules.jl
+++ src/PyCallChainRules.jl
@@ -12,6 +12,8 @@ struct PyAdaptor{T} end
 Adapt.adapt_storage(to::PyAdaptor{T}, x::AbstractArray) where {T} = convert(Array, x)
 Adapt.adapt_storage(to::PyAdaptor{T}, x::StridedArray) where {T} = x
 Adapt.adapt_storage(to::PyAdaptor{<:AbstractArray}, x::FillArrays.AbstractFill) = collect(x)
+# Handle views
+Adapt.adapt_structure(to::PyAdaptor{<:AbstractArray}, x::A) where {A <: SubArray}  = collect(x)
 
 ### XXX: what's a little piracy between us
 fmap(f, x::ChainRulesCore.Tangent) = fmap(f, x.backing)
--- src/cuda.jl
+++ src/cuda.jl
@@ -1,3 +1,4 @@
 Adapt.adapt_storage(to::PyAdaptor{T}, x::CUDA.AnyCuArray) where {T} = CUDA.CuArray(x)
 Adapt.adapt_storage(to::PyAdaptor{T}, x::CUDA.StridedCuArray) where {T} = x
 Adapt.adapt_storage(to::PyAdaptor{<:CUDA.CuArray}, x::FillArrays.AbstractFill) = CUDA.cu(collect(x))
+Adapt.adapt_structure(to::PyAdaptor{<:CUDA.CuArray}, x::A) where {A <: SubArray}  = CUDA.cu(collect(x))
